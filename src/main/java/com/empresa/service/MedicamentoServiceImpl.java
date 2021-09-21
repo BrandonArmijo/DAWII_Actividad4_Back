210f9d00a32b1bbdb44a66acbfa39bb354078567
BrandonArmijo <brandon.armijo96@gmail.com>
2021-09-20 22:30:16 -0500

    Commit mio Medicamento

---
+++ src/main/java/com/empresa/service/MedicamentoServiceImpl.java
@@ -0,0 +1,37 @@
+package com.empresa.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.empresa.entity.Medicamento;
+import com.empresa.repository.MedicamentoRepository;
+
+@Service
+public class MedicamentoServiceImpl implements MedicamentoService{
+	
+	@Autowired
+	private MedicamentoRepository repository;
+
+	@Override
+	public Medicamento insertaActualizaMedicamento(Medicamento obj) {
+		return repository.save(obj);
+	}
+
+	@Override
+	public List<Medicamento> listaMedicamento() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Medicamento> buscaPorId(int idMedicamento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
